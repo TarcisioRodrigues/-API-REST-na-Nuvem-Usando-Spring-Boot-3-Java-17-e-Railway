f45bc4c4b8d53c2a36241fa0c5fdac130be9d5a4
Tarcisio Rodrigues <tarcisiorodrigues454@gmail.com>
2025-03-22 12:13:57 -0300

    cors

--- src/main/java/DesafioAPIRest/ApiRest/ApiRestApplication.java
+++ src/main/java/DesafioAPIRest/ApiRest/ApiRestApplication.java
@@ -1,13 +1,19 @@
 package DesafioAPIRest.ApiRest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers ={@Server(url = "/",description = "Default Server URL")} )
 @SpringBootApplication
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(ApiRestApplication.class, args);
+
 	}
 
 }
